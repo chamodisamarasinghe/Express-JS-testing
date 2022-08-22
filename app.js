ee4387f64beebca43570fe87731280495ffab336
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-22 15:31:49 +0530

    Updated files

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
 const express = require('express')
+const customer = require('./routes/customer')
+const item = require('./routes/item')
 const app = express()
 const port = 4000
 
+
+
 app.use(express.json())
+app.use('/customer', customer)
+app.use('/item',  item)
 
-app.get('/',(req,res) =>{
-    console.log('get request coming!');
-    res.send('get request came for / route')
+
+app.get('/', (req, res) => {
+    console.log('get request comming! ');
+    res.send('get req came for / route')
 })
 
 
-app.post('/', (req,res) =>{
-    res.send('<h1>get request came for / route</h1>')
-})
 
 
-app.get('/customer',(req,res) =>{
-    console.log('get request come');
-    res.send('<h1>get request came !</h1>')
-})
-
-
-app.post('/customer',(req,res) =>{
-    console.log(req.body);
-    res.send('<h1>Customer post request came !</h1>')
-})
-
-app.delete('/customer',(req,res) =>{
-    console.log(req.query.id);
-    res.send('delete req came from customer')
-})
-
-app.get('/cart',(req,res) =>{
-    console.log(req.query.id);
-    res.send('cart get request')
-})
-
-app.listen(port, () =>{
-    console.log(`app listening on port ${port}`);
-
+app.listen(port, () => {
+    console.log(`app starting on ${port}`);
 })
