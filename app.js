887112a01d1d9747d8a85e38caa43f28917fcd45
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-22 10:46:37 +0530

    Updated files

--- app.js
+++ app.js
@@ -9,12 +9,25 @@ app.get('/',(req,res) =>{
     res.send('get request came for / route')
 })
 
-app.get('/customer',(req,res) =>{
-    console.log('get request come');
-    res.send('<h1>get request came</h1>')
+
+app.post('/', (req,res) =>{
+    res.send('<h1>get request came for / route</h1>')
 })
 
 
+app.get('/customer',(req,res) =>{
+    console.log('get request come');
+    res.send('<h1>get request came !</h1>')
+})
+
+
+app.post('/customer',(req,res) =>{
+    res.send('<h1>Customer post request came !</h1>')
+})
+
+app.delete('/customer',(req,res) =>{
+    res.send('delete req came from customer')
+})
 
 app.listen(port, () =>{
     console.log(`app listening on port ${port}`);
