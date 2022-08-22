940fdf9d9bc9edcd53be97481e446571c3c44c5d
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-22 12:06:39 +0530

    Added files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-
+app.use(express.json())
 
 app.get('/',(req,res) =>{
     console.log('get request coming!');
@@ -22,11 +22,18 @@ app.get('/customer',(req,res) =>{
 
 
 app.post('/customer',(req,res) =>{
+    console.log(req.body);
     res.send('<h1>Customer post request came !</h1>')
 })
 
 app.delete('/customer',(req,res) =>{
+    console.log(req.query.id);
     res.send('delete req came from customer')
+})
+
+app.get('/cart',(req,res) =>{
+    console.log(req.query.id);
+    res.send('cart get request')
 })
 
 app.listen(port, () =>{
