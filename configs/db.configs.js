f2b41bd95d7eeaa28295234585019cc11e736480
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-23 19:13:54 +0530

    Updated files

---
+++ configs/db.configs.js
@@ -0,0 +1,8 @@
+module.exports ={
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'Chamodi@123',
+        database: 'express'
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,27 @@
 const express = require('express')
+const mysql = require('mysql')
+const db = require('../configs/db.configs')
+
+const connection = mysql.createConnection(db.database)
+
+connection.connect(function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to the MySQL server');
+        var userTableQuery = "CREATE TABLE IF NOT EXISTS users (id VARCHAR(255) PRIMARY KEY, name VARCHAR(255), username VARCHAR(255))"
+        connection.query(userTableQuery, function (err, result) {
+            // if (err) throw err;
+            // console.log(result); //
+            // console.log("table created");
+            if (result.warningCount === 0) {
+                console.log("User table created!");
+            }
+        })
+    }
+
+})
+
 
 const router = express.Router()
 
