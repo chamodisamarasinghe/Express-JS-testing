59fb264981b22a7275bd67c2ef88ff1e21b726d7
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-25 01:15:57 +0530

    Updated files

--- routes/item.js
+++ routes/item.js
@@ -1,21 +1,57 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+const Item = require('../models/item.models')
 
 app.use(express.json())
 
-router.get('/', (req, res) => {
-    res.send('item get all')
+router.get('/', async (req, res) => {
+    try {
+        const items = await Item.find()
+        // res.send(items)
+        res.json(items)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
 })
 
 
-
-router.post('/', (req, res) => {
-    res.send(req.body)
+router.get('/:id', async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id)
+        res.json(item)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
 })
 
-router.delete('/', (req, res) =>{
-    res.send(req,query.id)
+
+router.post('/', async (req, res) => {
+    const item = new Item({
+        code: req.body.code,
+        description: req.body.description,
+        price: req.body.price,
+        qty: req.body.qty
+    })
+
+    try {
+        const response = await item.save()
+        // res.send(response)
+        res.json(response)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id)
+        const response = await item.remove()
+
+        res.json(response)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
 })
 
 
